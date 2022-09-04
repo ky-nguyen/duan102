d890f1f96ec2b263185a26b33fe5d7a9418c66ff
ky-nguyen <kynugyen00710011999@gmail.com>
2022-09-04 20:06:07 +0700

    first commit

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from tkinter import E
 from unicodedata import category
 from django.shortcuts import render, get_object_or_404
+
+from carts.models import CartItem
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+
 # Create your views here.
 
 
@@ -30,10 +34,13 @@ def product_detail(request, category_slug, product_slug):
     try:
         #TODO: category in store, slug in category app
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
     except Exception as e:
         raise e 
     
     context = {
-        'single_product': single_product,
+        'single_product'    : single_product,
+        'in_cart'           : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
