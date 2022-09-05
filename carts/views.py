f689f3bc55b5a6ea90ad818c94dd21f1c4151df4
ky-nguyen <kynugyen00710011999@gmail.com>
2022-09-05 15:57:53 +0700

    fix products warning, empty cart in views.py app store and views.py carts

--- carts/views.py
+++ carts/views.py
@@ -6,7 +6,7 @@ from carts.models import CartItem
 from store.models import Product
 from .models import  Cart, CartItem
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -68,6 +68,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -75,7 +77,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectNotExists:
+    except ObjectDoesNotExist:
         pass #
     
     context = {
